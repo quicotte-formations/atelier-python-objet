35226c50ad19776fb71e705ae8f69d8522e711e2
quicotte-formation <quicotte@gmail.com>
2023-11-10 16:57:33 +0100

    Fix tests unitaires

--- _FICHIERS/testing.py
+++ _FICHIERS/testing.py
@@ -13,7 +13,7 @@ class ATester(unittest.TestCase):
     def test_div_par_zero_pas_ok(self):
 
         with self.assertRaises(Exception):
-            c=2/2
+            c=2/0
 
     def test_b(self):
         a=1
