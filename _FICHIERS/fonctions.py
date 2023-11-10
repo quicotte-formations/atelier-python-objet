c5a6ef19aa81d292b9f0c77c5fc5ff530dc59b8e
quicotte-formation <quicotte@gmail.com>
2023-11-10 16:54:37 +0100

    Fix tests unitaires

---
+++ _FICHIERS/fonctions.py
@@ -0,0 +1,4 @@
+def transfert(cpt_a, cpt_b, montant):
+    if montant>10000:
+        return False
+    return True
--- _FICHIERS/testing.py
+++ _FICHIERS/testing.py
@@ -1,7 +1,19 @@
 import unittest
+from fonctions import *
 
 class ATester(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.data = range(1,10000000)
+
+    def test_data_contient_1_et_2(self):
+        self.assertIn(1, self.data)
+        self.assertIn(2, self.data)
+
     def test_div_par_zero_pas_ok(self):
+
+        self.data.remove(2)
+
         with self.assertRaises(Exception):
             c=2/0
 
@@ -10,3 +22,19 @@ class ATester(unittest.TestCase):
         b=2
         c=a/b
         self.assertEquals(0.5,c)
+
+    def test_c(self):
+        self.assertTrue(1==1)
+
+    def test_transfert_de_1000_OK(self):
+        res = transfert(1,2,1000)
+
+        self.assertTrue(res)
+
+    def test_transfert_de_100000000_KO(self):
+        res = transfert(1, 2, 100000000)
+
+        self.assertFalse(res)
+        self.assertEquals(False, res)
+        self.assertIn(2, [1,2,3])
+
